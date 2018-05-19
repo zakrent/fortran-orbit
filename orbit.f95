2caa2e075b7f21a9ce3144b3d630826ecfde2920
zakrent <szymon.zakrent@gmail.com>
2018-05-19 22:07:09 +0200

    Improved rk4

--- orbit.f95
+++ orbit.f95
@@ -1,12 +1,12 @@
 module params
         implicit none
-        real, parameter :: G = 6.674e-11               ! Grav constant   [m^3*kg^(−1)⋅s^(−2)]
-        real, parameter :: M = 6e24                    ! Planet mass     [kg]
-        real, parameter :: dt = 0.1                    ! Step size       [s]
-        integer, parameter :: n = 1e7                  ! Number of steps
+        real(kind=8), parameter :: G = 6.674e-11               ! Grav constant   [m^3*kg^(−1)⋅s^(−2)]
+        real(kind=8), parameter :: M = 6e24                    ! Planet mass     [kg]
+        real(kind=8), parameter :: dt = 0.01                 ! Step size       [s]
+        integer, parameter :: n = 5e7                  ! Number of steps
 
-        real, parameter, dimension(2) :: pos_0 = (/0.0, 7.0e6/)! Initial position [m]
-        real, parameter, dimension(2) :: vel_0 = (/10.0e3, 0.0/)! Initial velocity [m/s]
+        real(kind=8), parameter, dimension(2) :: pos_0 = (/0.0, 7.0e6/)! Initial position [m]
+        real(kind=8), parameter, dimension(2) :: vel_0 = (/10.0e3, 0.0/)! Initial velocity [m/s]
 
 end module params
 
@@ -14,23 +14,10 @@ module model_vars
         use params
 
         implicit none
-        real, dimension(2) :: cur_pos 
-        real, dimension(2) :: cur_vel
+        real(kind=8), dimension(2) :: cur_pos 
+        real(kind=8), dimension(2) :: cur_vel
 
-        real, dimension(n, 2) :: pos
-contains
-        function calacc (pos) result(acc)
-                implicit none
-
-                real, dimension(2) :: acc
-
-                real, dimension(2) :: pos
-                real, dimension(2) :: dir
-                real :: r
-
-                r = hypot(pos(1), pos(2))
-                acc = -G*M*pos/((r)**3)
-        end function calacc
+        real(kind=8), dimension(n, 2) :: pos
 end module model_vars
 
 program orbit
@@ -81,26 +68,35 @@ subroutine step
 
         implicit none
 
-        real, dimension(2) :: k_v1, k_v2, k_v3, k_v4
-        real, dimension(2) :: k_r1, k_r2, k_r3, k_r4
+        real(kind=8), dimension(2) :: k_v1, k_v2, k_v3, k_v4
+        real(kind=8), dimension(2) :: k_r1, k_r2, k_r3, k_r4
 
-        k_v1 = calacc(cur_pos)
-        k_r1 = cur_vel
-
-        k_v2 = calacc(cur_pos+k_r1*dt/2)
-        k_r2 = cur_vel+k_v1*dt/2
-
-        k_v3 = calacc(cur_pos+k_r2*dt/2)
-        k_r3 = cur_vel+k_v2*dt/2
-
-        k_v4 = calacc(cur_pos+k_r3*dt)
-        k_r4 = cur_vel+k_r3*dt
+        call rk4_step(cur_vel          , cur_pos          , k_v1, k_r1)
+        call rk4_step(cur_vel+k_v1*dt/2, cur_pos+k_r1*dt/2, k_v2, k_r2)
+        call rk4_step(cur_vel+k_v2*dt/2, cur_pos+k_r2*dt/2, k_v3, k_r3)
+        call rk4_step(cur_vel+k_v3*dt  , cur_pos+k_r3*dt  , k_v4, k_r4)
 
         cur_pos = cur_pos + dt/6*(k_r1+2*k_r2+2*k_r3+k_r4)
         cur_vel = cur_vel + dt/6*(k_v1+2*k_v2+2*k_v3+k_v4)
 
 end subroutine step
 
+subroutine rk4_step(vin, pin, vout, pout)
+        use params
+        use model_vars
+        implicit none
+
+        real(kind=8), dimension(2), intent(in) :: vin, pin
+        real(kind=8), dimension(2), intent(out) :: vout, pout
+
+        real(kind=8), dimension(2) :: dir
+        real(kind=8) :: r
+
+        r = hypot(pin(1), pin(2))
+        vout = -G*M*pin/((r)**3)
+
+        pout = vin 
+end subroutine rk4_step
 
 subroutine save_data
         use params
